566a2797fe1e46d8e4eb41ca1fa785088c51460a
ct0r <ct0r@protonmail.com>
2019-07-28 23:31:21 +0200

    Add JsonLdError struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod compact;
+mod error;
 mod expand;
 mod flatten;
 mod from_rdf;
 mod to_rdf;
 
 pub use compact::compact;
+pub use error::{JsonLdError, JsonLdErrorCode};
 pub use expand::expand;
 pub use flatten::flatten;
 pub use from_rdf::from_rdf;
